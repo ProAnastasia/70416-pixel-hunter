5d0b9b9ece7575a7a5bdf3a47e2914b89672e3c9
ProAnastasia <iluvala@bk.ru>
2018-03-27 22:33:42 +0300

    Toggle screens with required combinations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /**
  * Describe required order of the screens
  * @const
+ * @readonly
  * @type {Array}
  */
 const SCREENS_ORDER = [`greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`];
@@ -22,15 +23,29 @@ const KeyCode = {
 const mainScreensContainer = document.querySelector(`main.central`);
 
 /**
+ * Index of the current screen
+ * @type {number}
+ */
+let currentScreenIndex = 0;
+
+/**
  * An array of the templates with screens
  * @type {Array}
  */
 const screens = [...document.querySelectorAll(`template`)];
 
+/**
+ * Clear content of the screens container
+ * @function
+ */
 const clearContainerContent = () => {
   mainScreensContainer.innerHTML = ``;
 };
 
+/**
+ * Show screen with number
+ * @param {Number} number
+ */
 const showScreen = (number) => {
   const currentScreen = screens.find((screen) => screen.id === SCREENS_ORDER[number]);
   const currentScreenContent = currentScreen.content.cloneNode(true);
@@ -39,4 +54,32 @@ const showScreen = (number) => {
   mainScreensContainer.appendChild(currentScreenContent);
 };
 
-showScreen(0);
+/**
+ * Set keyup handler and show screens accordingly
+ * @param {Boolean} altKey
+ * @param {Number} keyCode
+ */
+const onKeyPressHandler = ({altKey, keyCode}) => {
+  if (!altKey) {
+    return;
+  }
+
+  switch (keyCode) {
+    case KeyCode.LEFT_ARROW:
+      if (currentScreenIndex > 0) {
+        --currentScreenIndex;
+        showScreen(currentScreenIndex);
+      }
+      break;
+    case KeyCode.RIGHT_ARROW:
+      if (currentScreenIndex < screens.length - 1) {
+        ++currentScreenIndex;
+        showScreen(currentScreenIndex);
+      }
+      break;
+  }
+};
+
+document.addEventListener(`keyup`, onKeyPressHandler);
+
+showScreen(currentScreenIndex);

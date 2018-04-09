8c91e26878d34da379e8e58f0a93a7e618d297b3
ProAnastasia <iluvala@bk.ru>
2018-04-09 12:26:27 +0300

    Enum for game points

--- js/modules/getResultScores.js
+++ js/modules/getResultScores.js
@@ -1,8 +1,20 @@
 /**
+ * Enum for game points
+ * @readonly
+ * @enum {number}
+ */
+const gamePoints = {
+  ANSWER_RIGHT: 100,
+  ANSWER_QUICK: 50,
+  ANSWER_SLOW: 50,
+  LIVE_VALUE: 50,
+  INITIAL_SCORE: 1150
+};
+/**
  * Get number of answers and lives, returns number of scores accordingly
  *  @return {Number}
  */
 export const getResultScores = () => {
-  return 1150;
+  return gamePoints.INITIAL_SCORE;
 };
 

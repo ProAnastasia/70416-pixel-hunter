af86e674ad6f81f7ca2fe42995bc3a9a425ac276
ProAnastasia <iluvala@bk.ru>
2018-04-09 23:56:38 +0300

    Start work with get result function

--- js/modules/getResultScores.js
+++ js/modules/getResultScores.js
@@ -1,20 +1,86 @@
 /**
+ * Initial game state description
+ * @type {Object}
+ */
+const INITIAL_GAME_STATE = {
+  totalPoints: 0,
+  lives: 3
+};
+/**
+ * Enum for game param
+ * @readonly
+ * @enum {number}
+ */
+const gameParams = {
+  QUESTION_TIME_FAST: 5,
+  QUESTION_TIME_SLOW: 15,
+  QUESTION_TOTAL_NUM: 10,
+  LIVES_MAX_NUM: 3
+};
+/**
  * Enum for game points
  * @readonly
  * @enum {number}
  */
 const gamePoints = {
-  ANSWER_RIGHT: 100,
+  ANSWER_CORRECT: 100,
   ANSWER_QUICK: 50,
   ANSWER_SLOW: 50,
   LIVE_VALUE: 50,
   INITIAL_SCORE: 1150
 };
 /**
- * Get number of answers and lives, returns number of scores accordingly
- *  @return {Number}
+ * Count scores logic
+ * @param {Array} answers
+ * @param {Number} lives
+ * @return {number}
  */
-export const getResultScores = () => {
-  return gamePoints.INITIAL_SCORE;
+const countScores = (answers, lives) => {
+  let scores = INITIAL_GAME_STATE.totalPoints;
+
+  answers.forEach((answer) => {
+    if (answer.value) {
+      scores += gamePoints.ANSWER_CORRECT;
+
+      if (answer.time <= gameParams.QUESTION_TIME_FAST) {
+        scores += gamePoints.ANSWER_QUICK;
+      }
+
+      if (answer.time > gameParams.QUESTION_TIME_SLOW) {
+        scores -= gamePoints.ANSWER_SLOW;
+      }
+    }
+  });
+
+  return scores * lives;
 };
 
+/**
+ * Get number of answers and lives, returns number of scores accordingly and handle errors
+ * @param {Array} answers
+ * @param {Number} livesNum
+ * @return {Number}
+ */
+export const getResultScores = (answers, livesNum) => {
+  if (answers.length < gameParams.QUESTION_TOTAL_NUM) {
+    return -1;
+  }
+
+  if (!Array.isArray(answers)) {
+    throw new Error(`answers param should have type: Array`);
+  }
+
+  if (typeof livesNum !== `number`) {
+    throw new Error(`livesNum param should have type: Number`);
+  }
+
+  if (livesNum < 0) {
+    throw new Error(`livesNum should be only positive number`);
+  }
+
+  if (livesNum > gameParams.LIVES_MAX_NUM) {
+    throw new Error(`livesNum should be less than maximum lives number`);
+  }
+
+  return countScores(answers, livesNum);
+};
--- js/modules/getResultScores.test.js
+++ js/modules/getResultScores.test.js
@@ -1,9 +1,20 @@
 import {assert} from 'chai';
 import {getResultScores} from './getResultScores';
 
+
+
 describe(`Check score counter`, () => {
-  it(`should return counted number of scores`, () => {
-    const result = getResultScores();
-    assert.equal(result, 1150);
+  it(`Should return return an error if answersNum type is not appropriate (Array)`, () => {
+    assert.throws(() => getResultScores({}, 1));
+    assert.throws(() => getResultScores(0, 1));
+    assert.throws(() => getResultScores(null, 1));
+    assert.throws(() => getResultScores(`test string`, 1));
+  });
+
+  it(`Should return return an error if livessNum type is not appropriate (Number)`, () => {
+    assert.throws(() => getResultScores([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], {}));
+    assert.throws(() => getResultScores([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], []));
+    assert.throws(() => getResultScores([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], null));
+    assert.throws(() => getResultScores([1, 2, 3, 4, 5, 6, 7, 8, 9, 0], `1`));
   });
 });

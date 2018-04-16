37784f635d51aef336885c2461ec8fe6834ea6ca
ProAnastasia <iluvala@bk.ru>
2018-04-16 23:13:46 +0300

    Change structure for images

--- js/data/images.js
+++ js/data/images.js
@@ -1,22 +1,32 @@
-export default {
-  paintings: [
-    // People
-    `https://k42.kn3.net/CF42609C8.jpg`,
-
-    // Animals
-    `https://k42.kn3.net/D2F0370D6.jpg`,
-
-    // Nature
-    `https://k32.kn3.net/5C7060EC5.jpg`
-  ],
-  photos: [
-    // People
-    `http://i.imgur.com/1KegWPz.jpg`,
-
-    // Animals
-    `https://i.imgur.com/DiHM5Zb.jpg`,
-
-    // Nature
-    `http://i.imgur.com/DKR1HtB.jpg`
-  ]
-};
+export default [
+  // People
+  {
+    src: `https://k42.kn3.net/CF42609C8.jpg`,
+    type: `painting`
+  },
+  // Animals
+  {
+    src: `https://k42.kn3.net/D2F0370D6.jpg`,
+    type: `painting`
+  },
+  // Nature
+  {
+    src: `https://k32.kn3.net/5C7060EC5.jpg`,
+    type: `painting`
+  },
+  // People
+  {
+    src: `http://i.imgur.com/1KegWPz.jpg`,
+    type: `photo`
+  },
+  // Animals
+  {
+    src: `https://i.imgur.com/DiHM5Zb.jpg`,
+    type: `photo`
+  },
+  // Nature
+  {
+    src: `http://i.imgur.com/DKR1HtB.jpg`,
+    type: `photo`
+  }
+];

dbdfb27ca01aec928205b2437f12ed076ebd52ae
ProAnastasia <iluvala@bk.ru>
2018-04-22 22:04:23 +0300

    Abstract view addition

---
+++ js/view/AbstractView.js
@@ -0,0 +1,30 @@
+import {createElementFromTemplate} from '../utils';
+
+export default class AbstractView {
+  constructor() {
+    if (new.target === AbstractView) {
+      throw new Error(`There is no opportunity to instantiate class: AbstractView`);
+    }
+  }
+
+  get template() {
+    throw new Error(`There is should be template here`);
+  }
+
+  render() {
+    return createElementFromTemplate(this.template);
+  }
+
+  bind() {
+    // Bind handlers
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = this.render();
+      this.bind();
+    }
+
+    return this._element;
+  }
+}
